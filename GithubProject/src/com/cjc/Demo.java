b0f2c3f02f7185e0c3455a1412f7195ba2f23e59
HP <HP@Pranali>
2025-02-10 21:29:17 +0530

    By Pranali

---
+++ GithubProject/src/com/cjc/Demo.java
@@ -0,0 +1,14 @@
+package com.cjc;
+
+public class Demo {
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello java selenium");
+
+	}
+    public void m1()
+    {
+    	System.out.println("m1 method");
+    }
+}
